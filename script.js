7642e461fb4360d975a2ec00c90c58be42d661e4
JulianKropa <jkropatschek@student.tgm.ac.at>
2023-11-04 19:14:44 +0100

    navbar emails
    navbar same width and right emails but not centered

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ async function toggleMobileMenu(menu) {
     var p = window.location.pathname;
     const hamburgerIcon = document.getElementById("hamburger-icon");
     hamburgerIcon.scrollIntoView({ behavior: "smooth" });
- if (p.length === 0  || p === "/" || p.match(/^\/?index/)) {
-    const scopes = document.getElementById("scopes-objectives");
-    scopes.classList.toggle('blur');
-    const team = document.getElementById("our-team");
-    team.classList.toggle('blur');
-    const collaborations = document.getElementById("collaborations");
-    collaborations.classList.toggle('blur');
-    const research = document.getElementById("research-basis");
-    research.classList.toggle('blur');
-    const milestones = document.getElementById("milestones");
-    milestones.classList.toggle('blur');
+    if (p.length === 0  || p === "/" || p.match(/^\/?index/)) {
+        const scopes = document.getElementById("scopes-objectives");
+        scopes.classList.toggle('blur');
+        const team = document.getElementById("our-team");
+        team.classList.toggle('blur');
+        const collaborations = document.getElementById("collaborations");
+        collaborations.classList.toggle('blur');
+        const research = document.getElementById("research-basis");
+        research.classList.toggle('blur');
+        const milestones = document.getElementById("milestones");
+        milestones.classList.toggle('blur');
     }
     if (p.length === 0  || p === "/" || p.match(/^\/?impressum/)) {
         const impressum = document.getElementById("impressum");
